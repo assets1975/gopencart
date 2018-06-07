4bc2a3826b0745f1e255bafab088a940f3768ad0
Asset Saparov <asset_s@mail.ru>
2018-06-07 10:40:27 +0600

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var es, log, logFile;
 
-const gulp = require('gulp-help')(require('gulp-param')(require('gulp'), process.argv));
-//const gulp = require('gulp-param')(require('gulp'), process.argv);
+// let es;
+let log;
+// let logFile;
+
+
+const gulp = require('gulp-help')(require('gulp-param')(require('gulp'), 
+             process.argv));
+// const gulp = require('gulp-param')(require('gulp'), process.argv);
 const template       = require('gulp-template');
-const rename         = require("gulp-rename");
+const rename         = require('gulp-rename');
 const replaceName    = require('gulp-replace-name');
-const gulpif         = require('gulp-if');
+// const gulpif         = require('gulp-if');
 const ext            = require('gulp-ext');
 const sass           = require('gulp-sass');
 const browserSync    = require('browser-sync');
 const cleanCSS       = require('gulp-clean-css');
 const autoprefixer   = require('gulp-autoprefixer');
-const bourbon        = require('node-bourbon');
+// const bourbon        = require('node-bourbon');
 const ftp            = require('vinyl-ftp');
 const gutil          = require('gulp-util' );
 const uglify         = require('gulp-uglify');
@@ -27,38 +32,33 @@ const plumber        = require('gulp-plumber');
 es = require('event-stream');
 log = require('gulp-util').log;
 
-String.prototype.capitalize = function () {
+String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
-logFile = function (es) {
-    return es.map(function (file, cb) {
-        log("----");
+logFile = function(es) {
+    return es.map(function(file, cb) {
+        log('----');
         return cb();
     });
 };
 
-var condition = function (file) {
-    // TODO: add business logic
-    //log(file.path);
-//  for(var f in file){
-//   console.log(f+"----"+file["_contents"]);   
-//  }
-    console.log(file.path);
-    return true;
-};
+// let condition = function(file) {
+//     console.log(file.path);
+//     return true;
+// };
 
 // run gulp gmodule --modulename test_module_name
-gulp.task('gmodule', 'generate opencart 2.3.x.x module', function (modulename) {
+gulp.task('gmodule', 'generate opencart 2.3.x.x module', function(modulename) {
     return gulp.src('templates/opencart_2_3/extension/module/**/*.*')
             .pipe(plumber())
-            .pipe(ext.replace('.php', '._php'))                  // должен ити  первым в потоке
+            .pipe(ext.replace('.php', '._php')) // должен ити  первым в потоке
             .pipe(replaceName(/modulename/g, modulename))
             .pipe(replaceName(/_/g, '\\'))
-            //.pipe(replaceName(/_php/g, 'php'))
-            //.pipe(logFile(es))
-            //.pipe(gulpif(condition,template({name:modulename})))
-            //.pipe(template({ModuleName:modulename.capitalize()}))
+            // .pipe(replaceName(/_php/g, 'php'))
+            // .pipe(logFile(es))
+            // .pipe(gulpif(condition,template({name:modulename})))
+            // .pipe(template({ModuleName:modulename.capitalize()}))
             .pipe(template({
                 name: modulename,
                 Name: modulename.capitalize()
@@ -74,16 +74,17 @@ gulp.task('gmodule', 'generate opencart 2.3.x.x module', function (modulename) {
 
 
 // run gulp gnewepayxtension --modulename test_module_name
-gulp.task('gnewepayxtension', 'generate opencart 2.3.x.x payment module', function (modulename) {
+gulp.task('gnewepayxtension', 
+          'generate opencart 2.3.x.x payment module', function(modulename) {
     return gulp.src('templates/opencart_2_3/extension/payment/**/*.*')
             .pipe(plumber())
-            .pipe(ext.replace('.php', '._php'))                  // должен ити  первым в потоке
+            .pipe(ext.replace('.php', '._php')) // должен ити  первым в потоке
             .pipe(replaceName(/modulename/g, modulename))
             .pipe(replaceName(/_/g, '\\'))
-            //.pipe(replaceName(/_php/g, 'php'))
-            //.pipe(logFile(es))
-            //.pipe(gulpif(condition,template({name:modulename})))
-            //.pipe(template({ModuleName:modulename.capitalize()}))
+            // .pipe(replaceName(/_php/g, 'php'))
+            // .pipe(logFile(es))
+            // .pipe(gulpif(condition,template({name:modulename})))
+            // .pipe(template({ModuleName:modulename.capitalize()}))
             .pipe(template({
                 name: modulename,
                 Name: modulename.capitalize()
@@ -98,13 +99,14 @@ gulp.task('gnewepayxtension', 'generate opencart 2.3.x.x payment module', functi
 );
 
 // создает новую страницу в common
-// нужно создать новую схему и указать путь common/newpage где newpage название новой страницы
+// нужно создать новую схему и указать путь
+// common/newpage где newpage название новой страницы
 
-gulp.task('gnewpage', 'generate opencart 2.3.x.x new page "common/newpage"', function (newpagename) {
+gulp.task('gnewpage', 'generate opencart 2.3.x.x new page "common/newpage"', function(newpagename) {
     console.log(newpagename);
     return gulp.src('templates/opencart_2_3/common/**/*.*')
             .pipe(plumber())    
-            .pipe(ext.replace('.php', '._php'))                  // должен ити  первым в потоке
+            .pipe(ext.replace('.php', '._php')) // должен ити  первым в потоке
             .pipe(replaceName(/newpagename/g, newpagename))
             .pipe(replaceName(/_/g, '\\'))
             .pipe(template({
@@ -123,18 +125,18 @@ gulp.task('gnewpage', 'generate opencart 2.3.x.x new page "common/newpage"', fun
 // Обновление страниц сайта на локальном сервере
 // Нужно указать proxy
 gulp.task('browser-sync', function() {
-	browserSync({
-		proxy: "http://localhost/opencart_pro/",
-		notify: false
-	});
+browserSync({
+    proxy: "http://localhost/opencart_pro/",
+	notify: false
+});
 });
 
 // Компиляция *.scss
 gulp.task('scss', function() {
     return gulp.src('dist/**/*.scss')
-        .pipe(plumber())    
+        .pipe(plumber())
 		.pipe(sourcemaps.init())
-        .pipe(sass()) //Скомпилируем                
+        .pipe(sass()) // Скомпилируем                
 		.pipe(autoprefixer(['last 15 versions']))
 		.pipe(cleanCSS().on('error',gutil.log))
         .pipe(sourcemaps.write())
